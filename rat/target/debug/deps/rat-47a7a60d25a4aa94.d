494aa9a4de36c65784413939f8916a6965f0f6b6
hagi017 <generativepassion@gmail.com>
2019-03-10 01:29:58 +0900

    remove target

--- rat/target/debug/deps/rat-47a7a60d25a4aa94.d
+++
@@ -1,5 +0,0 @@
-/home/hagiwara/workspace/linux-programming-in-rust/rat/target/debug/deps/rat-47a7a60d25a4aa94: src/main.rs
-
-/home/hagiwara/workspace/linux-programming-in-rust/rat/target/debug/deps/rat-47a7a60d25a4aa94.d: src/main.rs
-
-src/main.rs:
--- rat/target/debug/rat.d
+++
@@ -1,1 +0,0 @@
-/home/hagiwara/workspace/linux-programming-in-rust/rat/target/debug/rat: /home/hagiwara/workspace/linux-programming-in-rust/rat/src/main.rs
